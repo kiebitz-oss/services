f8b6631d5b36912f08114ba83735149520e15c1f
Jürgen Peters <juergen.peters@antei.de>
2021-12-04 16:46:50 +0100

    date range options for getProviderAppointments - Issue #16

--- forms/appointments.go
+++ forms/appointments.go
@@ -794,6 +794,22 @@ var GetProviderAppointmentsDataForm = forms.Form{
 				forms.IsTime{Format: "rfc3339"},
 			},
 		},
+		{
+			Name: "fromDate",
+			Validators: []forms.Validator{
+				forms.IsOptional{Default: "0000-00-00"},
+				forms.MatchesRegex{Regex: regexp.MustCompile(`^(|\d\d\d\d-\d\d-\d\d)$`)},
+				forms.IsString{},
+			},
+		},
+		{
+			Name: "toDate",
+			Validators: []forms.Validator{
+				forms.IsOptional{Default: "9999-99-99"},
+				forms.MatchesRegex{Regex: regexp.MustCompile(`^(|\d\d\d\d-\d\d-\d\d)$`)},
+				forms.IsString{},
+			},
+		},
 	},
 }
 
